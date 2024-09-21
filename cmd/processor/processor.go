39fe64786ccc01e67eb3f2ef053df7e003fb04e9
somenko <somenko@sveak.com>
2024-09-21 16:22:55 +0300

    logger improvements

--- cmd/processor/processor.go
+++ cmd/processor/processor.go
@@ -1,32 +1,45 @@
 package main
 
 import (
+	"context"
 	"log"
-	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 	"xenking_test_1/internal/config"
 	internalLogger "xenking_test_1/internal/logger"
 )
 
 func main() {
-	if err := run(); err != nil {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
+func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	cfg, err := config.MustSetup(os.Getenv("APP_ENVIRONMENT"))
 	if err != nil {
 		return err
 	}
 
-	logger := internalLogger.Must(cfg.Environment)
-	logger.Info("starting...", slog.String("name", cfg.Name), slog.String("env", cfg.Environment))
+	logger := internalLogger.MustSetup(*cfg)
+	logger.Info("starting...")
 
-	// graceful shutdown
 	// line provider client
 	// storage
 	// workers
+
+	logger.Info("running")
+
+	select {
+	case <-ctx.Done():
+		stop()
+		logger.Info("shutting down")
+	}
 
 	return nil
 }
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -3,14 +3,19 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"xenking_test_1/internal/config"
 )
 
-func Must(env string) *slog.Logger {
+func MustSetup(cfg config.Config) *slog.Logger {
 	var handler slog.Handler
-	switch env {
+	switch cfg.Environment {
 	default:
 		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})
+		logger := slog.New(handler)
+		return logger.With(slog.Group(
+			"app",
+			slog.String("name", cfg.Name),
+			slog.String("env", cfg.Environment),
+		))
 	}
-
-	return slog.New(handler)
 }
